5669bb8868c309c457224fdcc10574935ac147ab
Tomi R <tomi@Tomis-MBP.home>
2018-05-16 10:39:03 +0300

    tehtavat 1.1-1.5

---
+++ osa_1/ekatehtava1.1-1.5/src/index.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+    return (
+        <h1> {props.kurssi} </h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+       <div>
+       <Osa osa={props.osat[0]}/>
+       <Osa osa={props.osat[1]}/>
+       <Osa osa={props.osat[2]}/>
+       </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+    )
+}
+
+const Yhteensa = (props) => {
+    const sum = props.osat[0].tehtavia + props.osat[1].tehtavia
+    + props.osat[2].tehtavia
+    return (
+        <p>yhteensä {sum} tehtävää </p>
+    )
+}
+
+const App = () => {
+    const kurssi = {
+      nimi: 'Half Stack -sovelluskehitys',
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14
+        }
+      ]
+    }
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi.nimi} />
+      <Sisalto osat={kurssi.osat} />
+      <Yhteensa osat={kurssi.osat} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
